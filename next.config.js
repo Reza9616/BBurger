95e1e07b8357b82ddc1d02f448242c69fc8ff6a9
rezarezaaliabadi-Rahaan <reza.rezaaliabadi@gmail.com>
2026-06-18 09:20:42 +0330

    Initial commit - B-Burger Project

---
+++ next.config.js
@@ -0,0 +1,27 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
+  },
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['mssql', 'tedious'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        dns: false,
+      };
+    }
+    return config;
+  },
+}
+
+module.exports = nextConfig
